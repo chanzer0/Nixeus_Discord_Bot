abad37b41c4cc04a5cb07c2e0bcda2fe58cdf6e3
Sean <seansailer28@gmail.com>
2018-04-26 19:34:14 -0500

    Added successful CSV read into a Pandas data frame. Also added 'start.bat' for easy launching of bot

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import discord
 from discord.ext import commands
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
 
-bot = commands.Bot(command_prefix='!', description='Nixeus, a Discord Bot')
+# URL for intraday stocks [1min] [5min] [15min] [30min] [60min]
+URL_stocks_intraday = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
 
-@bot.event
+
+# Our bot, Nixeus
+Nixeus = commands.Bot(command_prefix='!', description='Nixeus, a Discord Bot')
+
+
+@Nixeus.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(Nixeus.user.name)
+    print(Nixeus.user.id)
     print("\n")
 
-@bot.command()
-async def chart(ctx, ticker: str, time: str):
-    await ctx.send(ticker + " " + time)
 
-bot.run('MzAwNDkzODY2MjY2NTI1Njk2.DcKn8g.nnGuJDJYq56vi11bVz--GEal3MA')
+@Nixeus.command()
+async def chart(ctx, ticker: str, time: str):
+    URL = URL_stocks_intraday + ticker + "&interval=" + time + "&apikey=JDXPDDU9XXF3GIHD"
+    data_frame = pd.read_csv(URL + "&datatype=csv")
+    print(data_frame)
+
+
+Nixeus.run('MzAwNDkzODY2MjY2NTI1Njk2.DcKn8g.nnGuJDJYq56vi11bVz--GEal3MA')
